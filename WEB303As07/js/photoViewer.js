700c5cc321c8f6ba4f2d6764d8a464f965225279
Inderpreet kaur <124931128+Inderpreet92@users.noreply.github.com>
2023-11-10 23:11:52 -0500

    Add files via upload

---
+++ WEB303As07/js/photoViewer.js
@@ -0,0 +1,10 @@
+// photoViewer.js
+(function ($) {
+    $.fn.photoViewer = function () {
+        // Photo viewer plugin code here
+        this.each(function () {
+            // Implement the photo viewer functionality for each element
+        });
+        return this; // Enables method chaining
+    };
+})(jQuery);
